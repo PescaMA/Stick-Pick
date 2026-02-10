677a6441b2b493e2b186a8b4fbfae293c627ee57
PescaMA <pescariu.mate@gmail.com>
2026-02-10 20:49:47 +0200

    start with 2D template

---
+++ src/theme/palette.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+/// #ddd369 = sandy
+pub const LABEL_TEXT: Color = Color::srgb(0.867, 0.827, 0.412);
+
+/// #fcfbcc = whitish
+pub const HEADER_TEXT: Color = Color::srgb(0.988, 0.984, 0.800);
+
+/// #ececec = grey
+pub const BUTTON_TEXT: Color = Color::srgb(0.925, 0.925, 0.925);
+/// #4666bf = blue (darkish)
+pub const BUTTON_BACKGROUND: Color = Color::srgb(0.275, 0.400, 0.750);
+/// #6299d1 = blue (lightish)
+pub const BUTTON_HOVERED_BACKGROUND: Color = Color::srgb(0.384, 0.600, 0.820);
+/// #3d4999 = blue (darker)
+pub const BUTTON_PRESSED_BACKGROUND: Color = Color::srgb(0.239, 0.286, 0.600);

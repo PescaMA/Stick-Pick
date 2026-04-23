55c3734d3d66917c61661282a9333deda5bfc148
Pesca (liquid) <pescariu.mate@gmail.com>
2026-04-23 18:28:58 +0300

    add 2 hitboxes with same behaiviour

--- game/src/player/physics.rs
+++ game/src/player/physics.rs
@@ -12,35 +12,75 @@ use crate::{
 
 pub(crate) fn plugin(app: &mut App) {
     app.insert_resource(Gravity(Vec2::new(0.0, GRAVITY)));
-    app.add_plugins(PhysicsPlugins::default());
+    app.add_plugins((PhysicsPlugins::default(), PhysicsDebugPlugin))
+        .insert_gizmo_config(
+            PhysicsGizmos {
+                aabb_color: Some(Color::WHITE),
+                ..default()
+            },
+            GizmoConfig::default(),
+        );
     app.add_systems(Update, add_avian_body);
 }
 
 fn add_avian_body(mut commands: Commands, new_players: Query<Entity, Added<Player>>) {
     for player_ent in new_players.iter() {
-        commands.entity(player_ent).insert((
-            MovementController { ..default() },
-            ScreenBlock,
-            (
+        commands
+            .entity(player_ent)
+            .insert((
+                MovementController { ..default() },
                 RigidBody::Dynamic, // affected by gravity/colissions
-                // Capsule prevents catching on tile edges
-                Collider::capsule(7.0, 8.0),
-                CollisionLayers::from_bits(3, 3), // we are in layer 1 and collide with layer 1
-                // LockedAxes::ROTATION_LOCKED,
-                Friction::new(0.1),
-                Restitution::new(0.3), // bounciness. 1 = perfectly elastic, 0 = no
-                LinearVelocity::ZERO,  // start stationary
-                CollidingEntities::default(), // track collisions
-                GravityScale(1.0),
-            ),
-            PointLight2d {
-                // for firefly lighting
-                color: Color::srgb(0.2, 0.0, 1.0),
-                range: PLAYER_SPRITE_SIZE * SPRITE_SCALE * 3.,
-                intensity: 4.0,
-                ..default()
-            },
-        ));
+                ScreenBlock,
+                PointLight2d {
+                    // for firefly lighting
+                    color: Color::srgb(0.6, 0.01, 0.7),
+                    range: PLAYER_SPRITE_SIZE * SPRITE_SCALE * 4.,
+                    intensity: 1.7,
+                    ..default()
+                },
+            ))
+            .with_children(|children| {
+                // Spawn the child colliders positioned relative to the rigid body
+                children.spawn((
+                    Name::new("PickHead"),
+                    Transform::from_xyz(0.0, 10.0, 0.0)
+                        .with_rotation(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2)),
+                    (
+                        // Capsule prevents catching on tile edges
+                        Collider::capsule(3.0, 13.0),
+                        ColliderTransform {
+                            translation: Vec2::new(20., -4.),
+                            ..Default::default()
+                        },
+                        CollisionLayers::from_bits(3, 3), // we are in layer 1 and collide with layer 1
+                        // LockedAxes::ROTATION_LOCKED,
+                        Friction::new(0.1),
+                        Restitution::new(0.5), // bounciness. 1 = perfectly elastic, 0 = no
+                        LinearVelocity::ZERO,  // start stationary
+                        CollidingEntities::default(), // track collisions
+                        GravityScale(1.0),
+                    ),
+                ));
+                children.spawn((
+                    Name::new("Handle"),
+                    Transform::from_xyz(0.0, -4.0, 0.0),
+                    (
+                        // Capsule prevents catching on tile edges
+                        Collider::capsule(2.0, 20.0),
+                        ColliderTransform {
+                            translation: Vec2::new(20., -4.),
+                            ..Default::default()
+                        },
+                        CollisionLayers::from_bits(3, 3), // we are in layer 1 and collide with layer 1
+                        // LockedAxes::ROTATION_LOCKED,
+                        Friction::new(0.1),
+                        Restitution::new(0.3), // bounciness. 1 = perfectly elastic, 0 = no
+                        LinearVelocity::ZERO,  // start stationary
+                        CollidingEntities::default(), // track collisions
+                        GravityScale(1.0),
+                    ),
+                ));
+            });
     }
 }
 
